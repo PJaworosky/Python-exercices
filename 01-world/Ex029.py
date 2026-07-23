602d62c7405f008ff7eee065f4b9a18ce37aa4d3
PJaworosky <paloma.jaworosky@gmail.com>
2026-07-23 13:45:07 -0300

    Complete exercise 29

--- 01-world/Ex029.py
+++ 01-world/Ex029.py
@@ -1,3 +1,17 @@
+# ----------------------------------------
+# Exercise 029 - Speed Radar
+# Curso em Vídeo - Python
+# Author: Paloma Jaworosky
+# ----------------------------------------
+
 #Escreva um programa que leia a velocidade de um carro
-#Se ele ultrapassar 80 km/h mostre uma msg dizendo que ele foi multado
+#Se ele ultrapassar 80 km/h, mostre uma msg dizendo que ele foi multado
 #A multa vai custar R$7,00 por cada km acima do limite
+
+velocidade = input('\33[32mQual a velocidade atual do carro? ')
+velocidade = int(velocidade)
+multa = int(velocidade - 80) *7
+if velocidade > 80:
+    print('\33[0;30;41mVocê excedeu o limite de velocidade de 80km/h\33[m')
+    print(' \33[1mO valor da multa será R$ {:.2f}'.format(multa))
+    print('\33[0;36mTenha um bom dia! DIRIJA COM SEGURANÇA!')
